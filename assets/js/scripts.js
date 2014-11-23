35089466fd69680c274bac9bbd68bd0dcbffbde2
Eli José Carrasquero [Laptop] <ielijose@gmail.com>
2014-11-23 11:53:46 -0430

    inicio

---
+++ assets/js/scripts.js
@@ -0,0 +1,8 @@
+
+
+function rest(){ 
+ document.getElementById("csss").style.color="black";
+ document.getElementById("csss").style.width="100%";
+ document.getElementById("csss").style.fontSize="150%";
+ document.getElementById("csss").style.backgroundColor="#EEE";
+}
