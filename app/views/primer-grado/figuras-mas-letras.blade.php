061c743301bf24f02b720b25120224e7f6fb0d9a
Eli José Carrasquero [PC] <ielijose@gmail.com>
2015-01-11 17:11:21 -0430

    Figuras mas letras

--- app/views/primer-grado/figuras-mas-letras.blade.php
+++ app/views/primer-grado/figuras-mas-letras.blade.php
@@ -2,6 +2,32 @@
 
 @section('css')
 <link rel="stylesheet" href="/css/primer-grado.css">
+
+<style>
+	
+[draggable] {
+	-moz-user-select: none;
+	-khtml-user-select: none;
+	-webkit-user-select: none;
+	user-select: none;
+	/* Required to make elements draggable in old WebKit */
+	-khtml-user-drag: element;
+	-webkit-user-drag: element;
+}
+dd {
+	padding: 5px 0;
+}
+.dnd {
+	-webkit-transition: -webkit-transform 0.2s ease-out;
+	-moz-transition: -moz-transform 0.2s ease-out;
+	-o-transition: -o-transform 0.2s ease-out;
+	-ms-transition: -ms-transform 0.2s ease-out;
+}
+
+.hide{
+	display: none;
+}
+</style>
 @endsection
 
 @section('content')
@@ -53,25 +79,41 @@
 
 			<div class="row">
 
-				<div id="imagen" class="grid grid_8 center" align="center">
-					<img src="" alt="" id="figura">
-					<hr>
-				</div>
+				<div id="imagen" class="grid grid_8 center" align="center"></div>
 
 
 				<div class="grid grid_8 center" align="center">
-					<a href="#" class="extrasize nicdark_width50 nicdark_btn nicdark_bg_blue medium nicdark_shadow nicdark_radius white nicdark_margin10">A</a>
-					<a href="#" class="extrasize nicdark_width50 nicdark_btn nicdark_bg_red medium nicdark_shadow nicdark_radius white nicdark_margin10">E</a>
-					<a href="#" class="extrasize nicdark_width50 nicdark_btn nicdark_bg_yellow medium nicdark_shadow nicdark_radius white nicdark_margin10">I</a>
-					<a href="#" class="extrasize nicdark_width50 nicdark_btn nicdark_bg_orange medium nicdark_shadow nicdark_radius white nicdark_margin10">O</a>
-					<a href="#" class="extrasize nicdark_width50 nicdark_btn nicdark_bg_green medium nicdark_shadow nicdark_radius white nicdark_margin10">U</a>
+					<hr>
+					<a href="#" class="dnd extrasize nicdark_width50 nicdark_btn nicdark_bg_blue medium nicdark_shadow nicdark_radius white nicdark_margin10" data-vocal="A">A</a>
+					<a href="#" class="dnd extrasize nicdark_width50 nicdark_btn nicdark_bg_red medium nicdark_shadow nicdark_radius white nicdark_margin10" data-vocal="E">E</a>
+					<a href="#" class="dnd extrasize nicdark_width50 nicdark_btn nicdark_bg_yellow medium nicdark_shadow nicdark_radius white nicdark_margin10" data-vocal="I">I</a>
+					<a href="#" class="dnd extrasize nicdark_width50 nicdark_btn nicdark_bg_orange medium nicdark_shadow nicdark_radius white nicdark_margin10" data-vocal="O">O</a>
+					<a href="#" class="dnd extrasize nicdark_width50 nicdark_btn nicdark_bg_green medium nicdark_shadow nicdark_radius white nicdark_margin10" data-vocal="U">U</a>
 				</div>
+
+				<div class="grid grid_8 hide" id="alerta">
+					<div class="nicdark_alerts nicdark_bg_red nicdark_radius nicdark_shadow">
+						<p class="white nicdark_size_medium"><i class="icon-cancel-circled-outline iconclose"></i>&nbsp;&nbsp;&nbsp;<strong>Opción incorrecta:</strong>&nbsp;&nbsp;&nbsp;Intenta de nuevo.</p>
+						<i class="icon-cancel-outline nicdark_iconbg right medium red"></i>
+					</div>
+				</div>
+
+				<div class="grid grid_8 hide nicdark_textevidence nicdark_width_percentage100" id="correcto">
+					<div class="nicdark_textevidence nicdark_relative"> <a href="#" class="nicdark_btn_icon nicdark_bg_green extrabig nicdark_radius white nicdark_absolute nicdark_shadow"><i class="icon-ok big"></i></a>
+						<div class="nicdark_activity nicdark_marginleft100">
+							<h1 style="font-size:60px;padding-top:10px">Correcto</h1>
+							<div class="nicdark_space20"></div> <a href="/" class="nicdark_btn grey next-word"><i class="icon-right-open-outline"></i> Siguiente</a> </div>
+							<div class="nicdark_space20"></div>
+							<div class="nicdark_space20"></div>
+						</div>
+					</div>
 
 				<div id="figuras-mas-letras" class="grid grid_6">
 					
 
 
 				</div>
+
 
 
 			</div>			
@@ -90,5 +132,93 @@
 @endsection
 
 @section('js')
-	<script src="/js/figuras-mas-letras.js"> </script>	
+<script src="/js/figuras-mas-letras.js"> </script>	
+<script>
+Element.prototype.hasClassName = function(name) {return new RegExp("(?:^|\\s+)" + name + "(?:\\s+|$)").test(this.className); };
+Element.prototype.addClassName = function(name) {if (!this.hasClassName(name)) {this.className = this.className ? [this.className, name].join(' ') : name; } };
+Element.prototype.removeClassName = function(name) {if (this.hasClassName(name)) {var c = this.className; this.className = c.replace(new RegExp("(?:^|\\s+)" + name + "(?:\\s+|$)", "g"), ""); } };
+
+var nivel = 0;
+(function() {
+	var cols_ = document.querySelectorAll('.dnd');
+	console.log(cols_)
+	var dragSrcEl_ = null;
+
+	this.handleDragStart = function(e) {
+		$("#alerta").addClass('hide');
+
+		e.dataTransfer.effectAllowed = 'move';
+		e.dataTransfer.setData('text/html', this.innerHTML);
+
+		dragSrcEl_ = this;
+
+    // this/e.target is the source node.
+    this.addClassName('moving');
+
+};
+
+this.handleDragOver = function(e) {
+
+	if (e.preventDefault) {
+      e.preventDefault(); // Allows us to drop.
+  }
+
+  e.dataTransfer.dropEffect = 'move';
+
+  return false;
+};
+
+this.handleDragEnter = function(e) {
+	this.addClassName('over');
+};
+
+this.handleDragLeave = function(e) {
+	this.removeClassName('over');
+};
+
+this.handleDrop = function(e) {
+	
+	e.preventDefault();
+	if (e.stopPropagation) {
+		e.stopPropagation(); 
+	}
+    // Don't do anything if we're dropping on the same dnd we're dragging.
+    if (dragSrcEl_ != this) {
+    	//if the type is equal
+    	if(dragSrcEl_.getAttribute('data-vocal') == this.getAttribute('data-vocal')){
+    		dragSrcEl_.remove();
+    		this.value = e.dataTransfer.getData('text/html');
+    		nivel++;
+    		if(nivel == 4){
+    			$("#correcto").removeClass('hide');
+    		}
+    		
+    	}else{
+    		$("#alerta").removeClass('hide');
+    	}
+    	
+    }
+    return false;
+};
+
+this.handleDragEnd = function(e) {
+    // this/e.target is the source node.
+    [].forEach.call(cols_, function (col) {
+    	col.removeClassName('over');
+    	col.removeClassName('moving');
+    });
+};
+
+[].forEach.call(cols_, function (col) {
+    col.setAttribute('draggable', 'true');  // Enable dnds to be draggable.
+    col.addEventListener('dragstart', this.handleDragStart, false);
+    col.addEventListener('dragenter', this.handleDragEnter, false);
+    col.addEventListener('dragover', this.handleDragOver, false);
+    col.addEventListener('dragleave', this.handleDragLeave, false);
+    col.addEventListener('drop', this.handleDrop, false);
+    col.addEventListener('dragend', this.handleDragEnd, false);
+});
+
+})();
+</script>
 @endsection
--- public/js/figuras-mas-letras.js
+++ public/js/figuras-mas-letras.js
@@ -12,114 +12,19 @@ $.get('/api/primer-grado/figuras-mas-letras', function(data) {
 
 /* put the object */
 
-function figurar(obj){
-	$("#figura").prop('src', obj.path);
-
-	var filename = obj.filename;
-	$(".letras").empty();
-	$("#resultado").empty();
-
-	var counter = 0;
-	var pieces = [];
-
-	$.each(obj.silabas , function(index, val) {		
-		pieces.push(val);
-		$(".letras").append('<input type="text" class="letra toFill" disabled>')		
-	});
-
-	shuffle(pieces);
-
-	$.each(pieces , function(index, piece) {		
-		$(".pieces").append('<span>'+piece+'</span>');
-	});
-
-
-
-
-	console.table(pieces);
-
-	$('input').on('touchstart', function () {
-		$(this).focus();
-		focused = $(this);
-		if(!focused.hasClass('toFill')){
-			focused.next('input').trigger('touchstart');
-		}
-	});
-
-	$('input.toFill').on('click', function () {
-		$(this).focus();
-		focused = $(this);
-	});
-
-	$(".letras input").on("keydown", function(e){
-		var unicode=e.keyCode? e.keyCode : e.charCode;
-
-		if(unicode == 8){
-			return true;
-		}
-
-		if(((unicode > 96 && unicode < 123) || (unicode > 64 && unicode < 91))){
-			if($(this).val().length > 0){
-				return false;
-			}
-			return true;
-		}	
-		return false;
-	})
-
-	$(".letras input").on("keyup", function(e){
-		var unicode=e.keyCode? e.keyCode : e.charCode
-		var completado = "";
-
-		if(unicode != 8){
-			$.each($(".letras input"), function(index, val) {
-				completado = completado + val.value;
-			});
-
-			if(focused.next('input').is('input')){
-				focused.next('input').trigger('touchstart');
-			}
-
-			if(completado.length == filename.length){
-				if(completado.toLowerCase() == filename.toLowerCase()){
-					correcto();
-				}else{
-					equivocado();
-				}
-			}
-		}
-	})
-
-}
+$.each(images, function(index, obj) {
+	$("#imagen").append('<img src="'+obj.path+'" id="figura" data-vocal="'+obj.vocal+'" class="dnd">');
+});
 var index = 0;
 figurar(images[index]);
 
-
-
 function correcto(){
-	$("input").prop("disabled", true);
-
 	$("#resultado").html('<div class="nicdark_textevidence nicdark_width_percentage100" id="correcto"> <div class="nicdark_textevidence nicdark_relative"> <a href="#" class="nicdark_btn_icon nicdark_bg_green extrabig nicdark_radius white nicdark_absolute nicdark_shadow"><i class="icon-ok big"></i></a> <div class="nicdark_activity nicdark_marginleft100"> <h1 style="font-size:60px;padding-top:10px">Correcto</h1> <div class="nicdark_space20"></div>  <a href="#" class="nicdark_btn grey next-word"><i class="icon-right-open-outline"></i> Siguiente</a> </div> <div class="nicdark_space20"></div><div class="nicdark_space20"></div></div> </div>');	
 }
 function equivocado(){
 
 	$("#resultado").html('<div class="nicdark_textevidence nicdark_width_percentage100" id="correcto"> <div class="nicdark_textevidence nicdark_relative"> <a href="#" class="nicdark_btn_icon nicdark_bg_red extrabig nicdark_radius white nicdark_absolute nicdark_shadow"><i class="icon-cancel big"></i></a> <div class="nicdark_activity nicdark_marginleft100"> <h1 style="font-size:60px;padding-top:10px">Equivocado</h1> <div class="nicdark_space20"></div>  <a href="#" class="nicdark_btn grey next-word"><i class="icon-right-open-outline"></i> Siguiente</a> </div> <div class="nicdark_space20"></div><div class="nicdark_space20"></div></div> </div>');
 }
-
-
-
-
-$("#resultado").on("click", ".next-word", function(event){
-	event.preventDefault();
-	index++;
-	if(index < 4){
-		adivinar(images[index]);
-	}else{
-		location.href = "/";
-	}
-	
-});
-
 
 /* FUNCTIONS */
 
