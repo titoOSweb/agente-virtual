a86628cac0e667224e2639660b3887c5950ce286
Eli José Carrasquero [PC] <ielijose@gmail.com>
2015-01-16 01:59:51 -0430

    Sumas y restas

--- app/views/segundo-grado/a-restar.blade.php
+++ app/views/segundo-grado/a-restar.blade.php
@@ -40,38 +40,66 @@
 	<!--start nicdark_container-->
 	<div class="nicdark_container nicdark_clearfix">
 
-		<div class="nicdark_space50"></div>
-
 		<div class="grid grid_8">
-			<div class="nicdark_space50"></div>
-
-			<h1 class="subtitle greydark">Restar:</h1>
-			<div class="nicdark_space20"></div>
-			<div class="nicdark_divider left small"><span class="nicdark_bg_yellow nicdark_radius"></span></div>
-			<div class="nicdark_space20"></div>
-
-			<div class="nicdark_space50"></div>	
-
 			<div class="row">
 
-				<div class="grid grid_10">
+				<div class="grid grid_8">
+
+
+					<div class="grid grid_3 ">
+						<div class="nicdark_alerts nicdark_bg_red nicdark_radius nicdark_shadow">
+							<p class="white nicdark_size_medium"><i class="icon-cancel-circled-outline iconclose"></i>&nbsp;&nbsp;&nbsp;<strong>Incorrectas:</strong>&nbsp;&nbsp;&nbsp;<span id="incorrectas">0</span></p>
+							<i class="icon-cancel-outline nicdark_iconbg right medium red"></i>
+						</div>
+					</div>
+
+					<div class="grid grid_3 right" style="float:right">
+						<div class="nicdark_alerts nicdark_bg_green nicdark_radius nicdark_shadow">
+							<p class="white nicdark_size_medium"><i class="icon-cancel-circled-outline iconclose"></i>&nbsp;&nbsp;&nbsp;<strong>Correctas:</strong>&nbsp;&nbsp;&nbsp;<span id="correctas">0</span></p>
+							<i class="icon-cancel-outline nicdark_iconbg right medium green"></i>
+						</div>
+					</div>
+
+
+				</div>
+
+				<div class="grid grid_8 hide nicdark_textevidence nicdark_width_percentage100" id="correcto">
+				<div class="nicdark_textevidence nicdark_relative"> <a href="#" class="nicdark_btn_icon nicdark_bg_green extrabig nicdark_radius white nicdark_absolute nicdark_shadow"><i class="icon-ok big"></i></a>
+					<div class="nicdark_activity nicdark_marginleft100">
+						<h1 style="font-size:60px;padding-top:10px">Puedes continuar</h1>
+						<div class="nicdark_space20"></div> <a href="/" class="nicdark_btn grey next-word"><i class="icon-right-open-outline"></i> Siguiente</a> 
+					</div>
+					<div class="nicdark_space20"></div>
+					<div class="nicdark_space20"></div>
+				</div>
+			</div>
+
+				<div class="grid grid_10" id="operandos">
+
+					
 
 					<div class="grid grid_8" align="center">
-						<div class="operacion">10 - 4 = ?</div>
+						<hr>
+						<div class="numeromm" id="n1"></div>
+						<a href="#" class="nicdark_width50 nicdark_btn nicdark_bg_green extrasize  nicdark_radius white nicdark_margin10">-</a>
+						<div class="numeromm" id="n2"></div>
+						<a href="#" class="nicdark_width50 nicdark_btn nicdark_bg_blue extrasize  nicdark_radius white nicdark_margin10">=</a>
+						<div class="numeromm">?</div>
 						<hr>
 					</div>
 
 					<div class="grid grid_6 fallas">
-						<div class="nicdark_alerts nicdark_bg_red nicdark_radius nicdark_shadow">
-							<p class="white nicdark_size_medium">&nbsp;&nbsp;&nbsp; 14 - 2 = ?</p>
-							<i class="icon-warning-empty nicdark_iconbg right medium red"></i>
+						<div class="grid grid_3">
+							<input type="text" class="grande">
+						</div>
+						<div class="grid grid_2">
+							<div class="nicdark_space20"></div>
+							<span id="validar" class="nicdark_btn nicdark_bg_green medium nicdark_radius white"><i class="icon-ok"></i>&nbsp;&nbsp; Validar</span>
 						</div>
 					</div>
-					<div class="grid grid_2 ">
+					<div class="grid grid_2" id="watch">
 
-						<div class="nicdark_margin100">
-							<span id="reloj" class="nicdark_btn nicdark_bg_orange medium nicdark_radius white"><i class="icon-stopwatch"></i>&nbsp;&nbsp;60 seg</span>
-						</div>
+						
 
 					</div>
 					
@@ -83,7 +111,7 @@
 			
 
 			<div class="nicdark_space50"></div>
-					
+
 		</div>
 		
 		<!--sidebar-->
@@ -97,4 +125,9 @@
 </section>
 <!--end section-->
 @endsection
+
+@section('js')
+<script src="/js/restar.js"></script>
+@endsection
+
 
---
+++ public/js/sumar.js
@@ -0,0 +1,129 @@
+"use strict";
+
+var Timer = {
+	seg : 40,
+	countdown: function( elementName, seconds )
+	{
+		var element, endTime, hours, mins, msLeft, time;
+
+		var w = $("#watch");
+		var randomId = makeid();
+		var tpl = '<div class="nicdark_margin100"> <span id="reloj" class="nicdark_btn nicdark_bg_orange medium nicdark_radius white"><i class="icon-stopwatch"></i>&nbsp;&nbsp; <span id="'+randomId+'">'+this.seg+' seg</span></span> </div>';
+
+		w.html(tpl);
+
+		function twoDigits( n )
+		{
+			return (n <= 9 ? "0" + n : n);
+		}
+
+		function updateTimer()
+		{
+			msLeft = endTime - (+new Date);
+			if ( msLeft < 1000 ) {
+				incorrecto();
+				generarOperacion();
+			} else {
+				time = new Date( msLeft );
+				hours = time.getUTCHours();
+				mins = time.getUTCMinutes();
+				element.innerHTML = twoDigits( time.getUTCSeconds() ) + ' seg';
+				setTimeout( updateTimer, time.getUTCMilliseconds() + 500 );
+			}
+		}
+
+		element = document.getElementById( randomId );
+		endTime = (+new Date) + 1000 * (seconds) + 500;
+		updateTimer();
+	}
+}
+
+var $n1 = $("#n1"),
+	$n2 = $("#n2"),
+	$incorrectas = $("#incorrectas"),
+	$correctas = $("#correctas"),
+	$mayor = $("#mayor"),
+	$menor = $("#menor"),
+	index = 0,
+	res=0;
+
+	function generarOperacion(){
+		var numeros = [];
+		res = 0;
+		var i = 0;
+		while(i<2){
+			var n = Math.floor((Math.random() * 10) + 1);
+			var igual = false;
+			$.each(numeros, function(index, el) {
+				if(el == n){
+					igual = true;
+				}
+			});
+			if(!igual){
+				i++;
+				numeros.push(n);
+			}
+		};
+		numeros.sort(function(a, b){return a-b});
+		$.each([$n1, $n2], function(index, el) {
+			el.text(numeros[index]);
+			res = res + numeros[index];
+		});
+		index++;
+		if(index == 10){
+			fin();
+		}
+
+		Timer.countdown( "timer", Timer.seg );
+	}
+
+	generarOperacion();
+
+
+	function validar(){
+
+	}
+/* TRIGGER´s */
+
+$("#validar").on("click", function(event){
+	event.preventDefault();
+
+	var escrito = $(".grande").val();
+
+	//alert(escrito + " > " + res);
+	
+	if(escrito == res){
+		correcto();
+	}else{
+		incorrecto();
+	}	
+	generarOperacion();
+	$(".grande").val('');
+})
+
+function correcto(){
+	$correctas.text(($correctas.text() * 1) + 1);
+}
+function incorrecto(){
+	$incorrectas.text(($incorrectas.text() * 1) + 1);
+}
+
+function fin(){
+	$("#operandos").remove();
+	$("#correcto").removeClass('hide');
+}
+/* TIMER */
+
+function makeid()
+{
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 5; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
+
+
